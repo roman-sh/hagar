cfed5d0269ebb8357fd7cad128be78c925b475c8
Roman Shmigelsky <shmigelsky@gmail.com>
2025-03-20 23:23:30 +0200

    mongo removed

--- app/main.js
+++ app/main.js
@@ -4,12 +4,13 @@ import db from './db/connection.js'
 
 
 const main = async () => {
-    const users = db.collection('users')
-    
-    const result = await users.findOne({
-        _id: 'custom-id-1',
-    })
-    log.info({ result }, 'Mongo Query Test')
+    try {
+        // Test the CouchDB connection with a simple query
+        const result = await db.get('custom-id-1', { revs_info: true }).catch(() => null)
+        log.info({ result }, 'CouchDB Query Test')
+    } catch (error) {
+        log.error(error, 'CouchDB query error')
+    }
 }
 
 
